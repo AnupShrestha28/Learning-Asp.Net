b7e48df0b1800dbe34a188a28aa291df4b88cffb
KIST Student3 <student3@sddb.student>
2022-03-10 09:23:26 +0545

    Initial Commit

---
+++ First Asp.Net Project/Controllers/firstController.cs
@@ -0,0 +1,21 @@
+﻿using First_Asp.Net_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace First_Asp.Net_Project.Controllers
+{
+    public class firstController : Controller
+    {
+        MainEntities db = new MainEntities();
+        // GET: first
+        public ActionResult Index()
+        {
+            List<emmployee> all_data = db.emmployees.ToList();
+            return View(all_data);
+        }
+       
+    }
+}
---
+++ First Asp.Net Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="First_Asp.Net_Project.MvcApplication" Language="C#" %>
