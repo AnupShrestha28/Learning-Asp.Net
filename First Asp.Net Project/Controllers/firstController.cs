fc636ecd008973c050e35f70ebf32fadb52d2375
AnupShrestha28 <anupstha0168@gmail.com>
2022-03-14 09:08:36 +0545

    Initial Commit

--- First Asp.Net Project/Controllers/firstController.cs
+++ First Asp.Net Project/Controllers/firstController.cs
@@ -1,6 +1,7 @@
 ﻿using First_Asp.Net_Project.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -40,6 +41,19 @@ namespace First_Asp.Net_Project.Controllers
             db.emmployees.Add(emmployee);
             db.SaveChanges();
             return RedirectToAction("Doctor");
+        }
+
+        public ActionResult Updatedata(emmployee emmployee)
+        {
+            db.Entry(emmployee).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            emmployee emmployee = db.emmployees.Find(id);
+          //emmployee emmployee = db.emmployees.FirstOrDefault(x => x.id == id);
+            return View(emmployee);
         }
     }
 }
